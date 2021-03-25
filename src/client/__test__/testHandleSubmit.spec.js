5523ffa1d1bfa30f7783e93e7ea09afbf0828bca
Omar <omarmuhammed1998.om@gmail.com>
2021-03-25 17:50:09 +0200

    unit testing for handleSubmit added

--- src/client/__test__/testHandleSubmit.spec.js
+++ src/client/__test__/testHandleSubmit.spec.js
@@ -1,7 +1,8 @@
-// to solve ReferenceError: regeneratorRuntime is not defined
-// https://knowledge.udacity.com/questions/174638
 import 'babel-polyfill'
+import { handleSubmit } from '../js/formHandler'
 
 describe('Client Test', () => {
-    // TODO: add your test cases to test client
+    test('Testing to see whether handleSubmit function is defined or not', () => {
+        expect(handleSubmit).toBeDefined()
+    })
 })
