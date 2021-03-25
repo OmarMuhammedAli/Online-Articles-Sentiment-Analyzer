382c280819b0310bbe6719179945c9e5a2ac2e3a
Omar <omarmuhammed1998.om@gmail.com>
2021-03-25 11:59:00 +0200

    Remote repo migration commenced

---
+++ src/client/__test__/testHandleSubmit.spec.js
@@ -0,0 +1,7 @@
+// to solve ReferenceError: regeneratorRuntime is not defined
+// https://knowledge.udacity.com/questions/174638
+import 'babel-polyfill'
+
+describe('Client Test', () => {
+    // TODO: add your test cases to test client
+})
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'minimal',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+        // TODO: configure workbox-webpack-plugin
+    ]
+}
