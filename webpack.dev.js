0892f7c143f57ad1ba84055dfa56a2278bdb1c7a
Omar <omarmuhammed1998.om@gmail.com>
2021-03-25 16:37:00 +0200

    setup for service workers added

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,39 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'minimal',
-    devServer: {
-        port: 8080
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/client/views/index.html',
-            filename: './index.html'
-        }),
-        new CleanWebpackPlugin({
-            dry: true,
-            verbose: true,
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-        // TODO: configure workbox-webpack-plugin
-    ]
-}
+  entry: "./src/client/index.js",
+  mode: "development",
+  devtool: "source-map",
+  stats: "minimal",
+  devServer: {
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
