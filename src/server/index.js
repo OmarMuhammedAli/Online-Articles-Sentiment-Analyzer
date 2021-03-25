2f9b0774355163bd5c3c9618cbcff3d569839ecd
Omar <omarmuhammed1998.om@gmail.com>
2021-03-25 15:33:30 +0200

    Created analysis endpoint which communicates with meaningcloud api

--- src/server/index.js
+++ src/server/index.js
@@ -1,54 +1,51 @@
 // TODO: Configure the environment variables
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const axios = require("axios");
+require("dotenv").config();
 
-const mockAPIResponse = require('./mockAPI.js')
+const app = express();
+const PORT = 8081;
 
-const PORT = 8081
-
-// TODO add Configuration to be able to use env variables
-
-const BASE_API_URL = 'https://api.meaningcloud.com/sentiment-2.1'
-
-// TODO: Create an instance for the server
-// TODO: Configure cors to avoid cors-origin issue
-// TODO: Configure express to use body-parser as middle-ware.
+app.use(cors());
+app.use(express.json());
 // TODO: Configure express static directory.
+// Change to dist
+app.use(express.static("src/client"));
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 // INFO: a route that handling post request for new URL that coming from the frontend
-app.post('/add-url', async (req, res) => {
-    try {
-        /* TODO:
-    1. GET the url from the request body
-    2. Build the URL it should be something like `${BASE_API_URL}?key=${MEAN_CLOUD_API_KEY}&url=${req.body.url}&lang=en`
-    3. Fetch Data from API
-    4. Send it to the client
-    5. REMOVE THIS TODO AFTER DOING IT 😎😎
-    server sends only specified data to the client with below codes
-     const sample = {
-       text: '',
-       score_tag : '',
-       agreement : '',
-       subjectivity : '',
-       confidence : '',
-       irony : ''
-     }
-  */
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+app.post("/analysis", async (req, res) => {
+  const url = req.body.url;
+  const endpoint = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${url}&lang=en`;
+  try {
+    const meaningcloudResponse = await axios(endpoint);
+    const data = {
+      text: meaningcloudResponse.data.sentence_list[0].text,
+      score_tag: meaningcloudResponse.data.score_tag,
+      agreement: meaningcloudResponse.data.agreement,
+      subjectivity: meaningcloudResponse.data.subjectivity,
+      confidence: meaningcloudResponse.data.confidence,
+      irony: meaningcloudResponse.data.irony,
+    };
+    res.send(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
 
 // designates what port the app will listen to for incoming requests
 app.listen(PORT, (error) => {
-    if (error) throw new Error(error)
-    console.log(`Server listening on port ${PORT}!`)
-})
+  if (error) throw new Error(error);
+  console.log(`Server listening on port ${PORT}!`);
+});
 
 // TODO: export app to use it in the unit testing
